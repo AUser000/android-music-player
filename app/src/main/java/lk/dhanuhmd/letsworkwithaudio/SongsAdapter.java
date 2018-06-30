0d2f04839c8144bdafad5e0a619bbc9a69a38bc4
AUser000 <dhanuhmd80@gmail.com>
2018-06-30 18:46:28 +0530

    add songs name list

---
+++ app/src/main/java/lk/dhanuhmd/letsworkwithaudio/SongsAdapter.java
@@ -0,0 +1,10 @@
+package lk.dhanuhmd.letsworkwithaudio;
+
+import android.widget.ArrayAdapter;
+
+/**
+ * Created by Acer on 30/06/2018.
+ */
+
+public class SongsAdapter extends ArrayAdapter {
+}
---
+++ app/src/main/java/lk/dhanuhmd/letsworkwithaudio/database/Permitions.java
@@ -0,0 +1,8 @@
+package lk.dhanuhmd.letsworkwithaudio.database;
+
+/**
+ * Created by Dhanushka Dharmasena on 30/06/2018.
+ */
+
+public class Permitions {
+}
