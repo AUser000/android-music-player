bff0aa1e0d3c4b19efadcf1acefab8991da29ff7
AUser000 <dhanuhmd80@gmail.com>
2018-07-02 22:20:39 +0530

    delete fucking database

--- app/src/main/java/lk/dhanuhmd/letsworkwithaudio/MainActivity.java
+++ app/src/main/java/lk/dhanuhmd/letsworkwithaudio/MainActivity.java
@@ -1,39 +1,35 @@
 package lk.dhanuhmd.letsworkwithaudio;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.pm.PackageManager;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteDatabase;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Toast;
 
-import java.io.File;
 import java.util.ArrayList;
 
-import lk.dhanuhmd.letsworkwithaudio.database.DBHelper;
-import lk.dhanuhmd.letsworkwithaudio.database.SongDb;
-import lk.dhanuhmd.letsworkwithaudio.database.Table;
-import lk.dhanuhmd.letsworkwithaudio.model.Genre;
 import lk.dhanuhmd.letsworkwithaudio.model.Song;
 
 public class MainActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener {
 
-    private Button pauseBtn, playBtn, stopBtn ;
-    SQLiteDatabase database;
+    private Button pauseBtn,stopBtn ;
     MediaPlayer player;
-    DBHelper dbHelper;
     ListView listView;
     ArrayList<Song> songsList;
     ArrayList<String> arrayNameList;
@@ -46,40 +42,64 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        dbHelper = new DBHelper(MainActivity.this);
         songsList = new ArrayList<>();
         arrayNameList = new ArrayList<>();
         listView = (ListView) findViewById(R.id.listView);
         swipeLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_container);
         swipeLayout.setOnRefreshListener(this);
         swipeLayout.setColorScheme(android.R.color.holo_red_light);
-
-        try {
-            if (android.os.Build.VERSION.SDK_INT > 23) {
+        if (android.os.Build.VERSION.SDK_INT > 23) {
+            if(!permissionGranted) {
+                checkPermission();
                 if(!permissionGranted) {
-                    checkPermission();
-                    if(!permissionGranted) {
-                        return;
-                    }
+                    return;
                 }
             }
-            database = dbHelper.getReadableDatabase();
-            Cursor cursor = database.rawQuery("SELECT * FROM MUSIC", null);
-            while (cursor.moveToNext()) {
-                Song temp = new Song();
-                temp.setSongName(cursor.getString(cursor.getColumnIndex("NAME")));
-                temp.setSongPath(cursor.getString(cursor.getColumnIndex("PATH")));
-                temp.setGenre(Genre.OTHER);
-                temp.setArtist("");
-                temp.setAlbum("");
-                songsList.add(temp);
-                arrayNameList.add(cursor.getString(cursor.getColumnIndex("NAME")));
-            }
-            ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_activated_1, arrayNameList );
-            listView.setAdapter(adapter);
-        } catch (Exception e) {
-            Toast.makeText(MainActivity.this, ""+e , Toast.LENGTH_SHORT).show();
         }
+
+        new Thread(new Runnable() {
+            public void run() {
+                    ContentResolver contentResolver = getContentResolver();
+                    Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                    String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+                    String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+                    Cursor cur = contentResolver.query(uri, null, selection, null, sortOrder);
+                    int count = 0;
+                    if(cur != null) {
+                        count = cur.getCount();
+                        if(count > 0) {
+                            while(cur.moveToNext()) {
+                                String data = cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DATA));
+                                arrayNameList.add(data);
+                            }
+
+                        }
+                    }
+                    cur.close();
+                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_activated_1, arrayNameList );
+                    listView.setAdapter(adapter);
+            }
+        }).start();
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String entry= (String) parent.getAdapter().getItem(position);
+                if ( player != null && player.isPlaying()) {
+                    player.stop();
+                }
+                if (android.os.Build.VERSION.SDK_INT > 23) {
+                    if(!permissionGranted) {
+                        checkPermission();
+                        if(!permissionGranted) {
+                            return;
+                        }
+                    }
+                }
+                player = MediaPlayer.create(MainActivity.this, Uri.parse(entry));
+                player.start();
+            }
+        });
 
         pauseBtn = (Button) findViewById(R.id.pauseBtn);
         pauseBtn.setOnClickListener(new View.OnClickListener() {
@@ -97,26 +117,6 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
             }
         });
 
-        playBtn = (Button) findViewById(R.id.plyBtn);
-        playBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                if ( player != null && player.isPlaying()) {
-                    player.stop();
-                }
-                if (android.os.Build.VERSION.SDK_INT > 23) {
-                    if(!permissionGranted) {
-                        checkPermission();
-                        if(!permissionGranted) {
-                            return;
-                        }
-                    }
-                }
-                player = MediaPlayer.create(MainActivity.this, /* Uri.parse(path)*/ R.raw.neked);
-                player.start();
-            }
-        });
-
         stopBtn = (Button) findViewById(R.id.stopBtn);
         stopBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -128,6 +128,7 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
         });
 
     }
+
 
     @Override
     public void onRefresh() {
@@ -142,41 +143,28 @@ public class MainActivity extends AppCompatActivity implements SwipeRefreshLayou
                         }
                     }
                 }
-                //String extStoreSd = System.getenv("SECONDARY_STORAGE");
-                //File fileDirSd = new File(extStoreSd);
-                //File dir for internal storage(programmatically external)
-                String extStoreDv = System.getenv("EXTERNAL_STORAGE");
-                File fileDirDv = new File(extStoreDv);
-
-                database = dbHelper.getWritableDatabase();
-                database.delete(Table.TABLE, null, null);
-
-                SongDb.getSongsToDb(fileDirDv, database);
-                //SongDb.getSongsToDb(fileDirSd, database);
-                database.close();
-                Toast.makeText(MainActivity.this, "fuck", Toast.LENGTH_SHORT).show();
-                try {
-                    database = dbHelper.getReadableDatabase();
-                    Cursor cursor = database.rawQuery("SELECT * FROM MUSIC", null);
-                    while (cursor.moveToNext()) {
-                        Song temp = new Song();
-                        temp.setSongName(cursor.getString(cursor.getColumnIndex("NAME")));
-                        temp.setSongPath(cursor.getString(cursor.getColumnIndex("PATH")));
-                        temp.setGenre(Genre.OTHER);
-                        temp.setArtist("");
-                        temp.setAlbum("");
-                        songsList.add(temp);
-                        arrayNameList.add(cursor.getString(cursor.getColumnIndex("NAME")));
+                ContentResolver contentResolver = getContentResolver();
+                Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                String selection = MediaStore.Audio.Media.IS_MUSIC + "!= 0";
+                String sortOrder = MediaStore.Audio.Media.TITLE + " ASC";
+                Cursor cur = contentResolver.query(uri, null, selection, null, sortOrder);
+                int count = 0;
+                arrayNameList = new ArrayList<>();
+                if(cur != null) {
+                    count = cur.getCount();
+                    if(count > 0) {
+                        while(cur.moveToNext()) {
+                            String data = cur.getString(cur.getColumnIndex(MediaStore.Audio.Media.DATA));
+                            arrayNameList.add(data);
+                        }
                     }
-                    ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_activated_1, arrayNameList );
-                    listView.setAdapter(adapter);
-                    Toast.makeText(MainActivity.this, "fuck", Toast.LENGTH_SHORT).show();
-                } catch (Exception e) {
-                    Toast.makeText(MainActivity.this, ""+e , Toast.LENGTH_SHORT).show();
                 }
+                cur.close();
+                ArrayAdapter adapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_activated_1, arrayNameList );
+                listView.setAdapter(adapter);
                 swipeLayout.setRefreshing(false);
             }
-        }, 5000);
+        }, 500);
     }
 
     public boolean isExternalStorageWritable() {
--- app/src/main/java/lk/dhanuhmd/letsworkwithaudio/Permitions.java
+++
@@ -1,9 +0,0 @@
-package lk.dhanuhmd.letsworkwithaudio;
-
-/**
- * Created by Dhanushka Dharmasena on 30/06/2018.
- */
-
-public class Permitions {
-
-}
